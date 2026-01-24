19efac80fdf4b98f1ac166bcb847b72c67dedd69
Joe Tannenbaum <joe@joe.codes>
2026-01-24 11:17:55 -0500

    Update from Maestro (#22)
    Co-authored-by: WendellAdriel <11641518+WendellAdriel@users.noreply.github.com>

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,7 +11,7 @@ class ExampleTest extends TestCase
 
     public function test_returns_a_successful_response()
     {
-        $response = $this->get('/');
+        $response = $this->get(route('home'));
 
         $response->assertOk();
     }
