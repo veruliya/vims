e2ca99d4a4fd41b6e00f10fb12d13e3637dc30d3
Racsi Beryl <veruliya@proton.me>
2026-05-13 14:09:49 +0700

    wip received report progress:
    - add alert dialog before clear selected

--- resources/js/pages/report/received/create/components/SelectedDrawer.tsx
+++ resources/js/pages/report/received/create/components/SelectedDrawer.tsx
@@ -1,4 +1,6 @@
-import { Description, Drawer, Button } from '@heroui/react';
+import { Description, Drawer, Button, AlertDialog } from '@heroui/react';
+
+import { useOverlayState } from '@heroui/react';
 
 import { useSelected } from '../contexts/SelectedContext';
 
@@ -7,39 +9,77 @@ import { SelectedListBox } from './SelectedListBox';
 export function SelectedDrawer() {
   const { form } = useSelected();
 
+  const clearSelectedState = useOverlayState();
+
   return (
-    <Drawer>
-      <Button>Show Selected</Button>
-      <Drawer.Backdrop>
-        <Drawer.Content>
-          <Drawer.Dialog>
-            <Drawer.Handle /> {/* Optional: Drag handle */}
-            <Drawer.CloseTrigger /> {/* Optional: Close button */}
-            <Drawer.Header>
-              <Drawer.Heading>
-                {`${form.data.storeItems.length} items selected`}
-              </Drawer.Heading>
-              <Description className="text-sm">
-                Set the quantity of the received items.
-              </Description>
-            </Drawer.Header>
-            <Drawer.Body>
-              <SelectedListBox />
-            </Drawer.Body>
-            <Drawer.Footer>
-              <div className="flex w-full justify-between">
-                <Button
-                  variant="danger"
-                  onPress={() => form.setData('storeItems', [])}
-                >
-                  Clear Selected
-                </Button>
-                <Button>Proceed</Button>
-              </div>
-            </Drawer.Footer>
-          </Drawer.Dialog>
-        </Drawer.Content>
-      </Drawer.Backdrop>
-    </Drawer>
+    <>
+      <Drawer>
+        <Button>Show Selected</Button>
+        <Drawer.Backdrop>
+          <Drawer.Content>
+            <Drawer.Dialog>
+              <Drawer.Handle /> {/* Optional: Drag handle */}
+              <Drawer.CloseTrigger /> {/* Optional: Close button */}
+              <Drawer.Header>
+                <Drawer.Heading>
+                  {`${form.data.storeItems.length} items selected`}
+                </Drawer.Heading>
+                <Description className="text-sm">
+                  Set the quantity of the received items.
+                </Description>
+              </Drawer.Header>
+              <Drawer.Body>
+                <SelectedListBox />
+              </Drawer.Body>
+              <Drawer.Footer>
+                <div className="flex w-full justify-between">
+                  <Button
+                    variant="danger"
+                    onPress={clearSelectedState.open}
+                    isDisabled={form.data.storeItems.length === 0}
+                  >
+                    Clear Selected
+                  </Button>
+                  <Button>Proceed</Button>
+                </div>
+              </Drawer.Footer>
+            </Drawer.Dialog>
+          </Drawer.Content>
+        </Drawer.Backdrop>
+      </Drawer>
+      <AlertDialog.Backdrop
+        isDismissable={true}
+        isOpen={clearSelectedState.isOpen}
+        onOpenChange={clearSelectedState.setOpen}
+      >
+        <AlertDialog.Container placement="center">
+          <AlertDialog.Dialog>
+            <AlertDialog.CloseTrigger /> {/* Optional: Close button */}
+            <AlertDialog.Header>
+              <AlertDialog.Icon /> {/* Optional: Status icon */}
+              <AlertDialog.Heading>Clear selected items?</AlertDialog.Heading>
+            </AlertDialog.Header>
+            <AlertDialog.Body>
+              <p>All selected items and quantity updates will be cleared.</p>
+            </AlertDialog.Body>
+            <AlertDialog.Footer>
+              <Button
+                slot="close"
+                variant="tertiary"
+              >
+                Cancel
+              </Button>
+              <Button
+                slot="close"
+                variant="danger"
+                onPress={() => form.setData('storeItems', [])}
+              >
+                Clear Selected
+              </Button>
+            </AlertDialog.Footer>
+          </AlertDialog.Dialog>
+        </AlertDialog.Container>
+      </AlertDialog.Backdrop>
+    </>
   );
 }
